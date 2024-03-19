8f5165a6978e0c56082887740c6f31cff909e156
Prajwal Dhawarikar <pdhawarikar@farelogix.com>
2024-03-19 12:01:02 +0530

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import SearchInput from "./SearchInput";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" padding={2}></Image>
+      <SearchInput></SearchInput>
+      <ColorModeSwitch />
     </HStack>
   );
 };
